90c0420dffa6008fa37e55f612bb6791e40021ab
do1linux <royhome1023@qq.com>
2025-11-04 11:11:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -176,7 +176,7 @@ class DirectBrowser:
             .set_argument("--disable-blink-features=AutomationControlled")
             .set_argument("--disable-dev-shm-usage")
             .set_argument("--disable-gpu")
-            .set_argument("--remote-debugging-port=9222")
+            .set_argument("--remote-debugging-port=0")
         )
         co.set_user_agent(
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
